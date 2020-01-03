2dab612dbe22c3e0b5be75f706412e3ebf700cea
lab302-8 <lab302-8@labeng.local>
2020-01-03 16:25:22 +0700

    lab4_3

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int findDivisor(int x)
+{  int y=2;
+    while (y>0)
+    {
+     if(x%y==0){
+         return y;}
+     else{
+         y++;}
+    }
+     }
+
+
+int main(){
+    cout << findDivisor(10) << "\n";
+    cout << findDivisor(97) << "\n";
+    cout << findDivisor(221) << "\n";
+    return 0;
+}
+
+
 
