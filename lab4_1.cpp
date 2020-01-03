d0b0edd1b4b4960452c64245a3f16f89fdede196
lab302-8 <lab302-8@labeng.local>
2020-01-03 16:22:13 +0700

    lab4_1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+double findmyresult(double x)
+{return (3*x*x*x)+(2*2.71828)+pow(2,(2*x+1))+pow((x*x)+1,0.5);}
+
+int main(){
+ double x , y;
+  cout << "enter X = ";
+  cin >> x;
+  cout << "Result y =" << findmyresult(x);
+
+  return 0;
+}
